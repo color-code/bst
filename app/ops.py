f3062d6b1a78df1a26218e7106f3cc645d45f6e9
njacobs5074 <nick@yoco.co.za>
2020-02-09 11:42:24 +0200

    Fixed broken tree_delete()

--- app/ops.py
+++ app/ops.py
@@ -22,61 +22,64 @@ def tree_search(node, data):
         return tree_search(node.right_child, data)
 
 
-def tree_minimum(node):
-    while node.left_child is not None:
-        node = node.left_child
-    return node
+def tree_minimum(x):
+    while x.left_child is not None:
+        x = x.left_child
+
+    return x
 
 
-def tree_maximum(node):
-    while node.right_child is not None:
-        node = node.right_child
-    return node
+def tree_maximum(x):
+    while x.right_child is not None:
+        x = x.right_child
+
+    return x
 
 
-def tree_successor(node):
-    if node.right_child is not None:
-        return tree_successor(node.right_child)
+def tree_successor(x):
+    if x.right_child is not None:
+        return tree_minimum(x.right_child)
+    else:
+        y = x.parent
+        while y is not None and x == y.right_child:
+            x = y
+            y = y.parent
 
-    y = node.parent
-    while y is not None and node == y.right_child:
-        node = y
-        y = y.parent
-
-    return y
+        return y
 
 
 def tree_insert(tree, data):
     y = None
     x = tree.root
-    node = Node(data=data)
+    z = Node(data=data)
 
     while x is not None:
         y = x
-        if node.data < x.data:
+        if z.data < x.data:
             x = x.left_child
         else:
             x = x.right_child
 
-    node.parent = y
+    z.parent = y
     if y is None:
-        tree.root = node
-    elif node.data < y.data:
-        y.left_child = node
+        tree.root = z
+    elif z.data < y.data:
+        y.left_child = z
     else:
-        y.right_child = node
+        y.right_child = z
 
 
 def tree_delete(tree, data):
 
-    node = tree_search(tree.root, data)
-    if node is None:
-        return
+    z = tree_search(tree.root, data)
 
-    if node.left_child is None or node.right_child is None:
-        y = node
+    if z is None:
+        raise LookupError(f"Failed to find {data}")
+
+    if z.left_child is None or z.right_child is None:
+        y = z
     else:
-        y = tree_successor(node)
+        y = tree_successor(z)
 
     if y.left_child is not None:
         x = y.left_child
@@ -93,8 +96,10 @@ def tree_delete(tree, data):
     else:
         y.parent.right_child = x
 
-    if y != node:
-        node.data = y.data
+    if y != z:
+        z.data = y.data
+
+    return y
 
 
 def tree_print(tree):
@@ -106,3 +111,16 @@ def _print_subtree(node):
         _print_subtree(node.left_child)
         print(node)
         _print_subtree(node.right_child)
+
+
+def tree_to_list(tree):
+    return _tree_to_list(tree.root, [])
+
+
+def _tree_to_list(node, tree_nodes):
+    if node is not None:
+        _tree_to_list(node.left_child, tree_nodes)
+        tree_nodes.append(node.data)
+        _tree_to_list(node.right_child, tree_nodes)
+
+    return tree_nodes
